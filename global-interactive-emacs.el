4ace5d9933a031d80ab832b5802b4c53d493a68d
jinqiqi <jinqiqi@ali-lurf.local>
2022-07-25 10:20:41 +0800

    init

---
+++ global-interactive-emacs.el
@@ -0,0 +1,49 @@
+(defvar global-interactive-default-command nil)
+
+(defvar global-interactive-extensions-init nil)
+
+(defun global-interactive-choose (prompt items &rest ignored)
+  "Like `completing-read' but instead use dmenu.
+Useful for system-wide scripts."
+  (with-temp-buffer
+    (thread-first
+      (cond
+       ((functionp items)
+        (funcall items "" nil t))
+       ((listp (car items))
+        (mapcar #'car items))
+       (t
+        items))
+      (string-join "\n")
+      string-trim
+      insert)
+    (shell-command-on-region
+     (point-min)
+     (point-max)
+     (pcase system-type
+       ('gnu/linux (format "rofi -dmenu -fuzzy -i -p '%s'" prompt))
+       ('darwin "choose -m"))
+     nil t "*global-interactive-choose error*" nil)
+    (string-trim (buffer-string))))
+
+(defun global-interactive-emacs ()
+  (interactive)
+  (setq completing-read-function #'global-interactive-choose)
+  (mapc #'funcall global-interactive-extensions-init)
+  (lexical-let* ((candidates (mapcar #'car global-interactive-default-command))
+                 (selected-item (completing-read "" candidates))
+                 (command (seq-filter (lambda (command) (string= selected-item (car command))) global-interactive-default-command)))
+    
+    (when command
+      (funcall (car (cdr (car command)))))))
+
+
+;; when you focus Emacs recover completing-read-function
+(defun window-selection-recover-completing-read-function ()
+  (setq completing-read-function #'completing-read-default))
+
+(add-function :after after-focus-change-function #'window-selection-recover-completing-read-function)
+
+(provide 'global-interactive-emacs)
+
+
---
+++ global-interactive-open-url.el
@@ -0,0 +1,60 @@
+(require 'global-interactive-emacs)
+(add-to-list 'global-interactive-default-command (list "Open Url" #'global-interactive-open-url))
+
+(defvar global-interactive-url-plist nil)
+
+(defvar global-interactive-url-yaml-path nil)
+
+(defun file-to-string (file)
+  "File to string function"
+  (with-temp-buffer
+    (insert-file-contents file)
+    (buffer-string)))
+
+(defun global-interactive-open-url-init ()
+  (when (and (not global-interactive-url-plist)
+             global-interactive-url-yaml-path)
+    (require 'yaml)
+    (setq global-interactive-url-plist (yaml-parse-string (file-to-string global-interactive-url-yaml-path)
+                                                    :object-type 'plist
+                                                    :sequence-type 'array
+                                                    :null-object :empty))))
+
+(defun global-interactive--keys-to-names (keys)
+  (mapcar (lambda (key) (string-remove-prefix ":" key)) (mapcar #'symbol-name keys)))
+
+(defun global-interactive--find-key (name keys)
+  (car (seq-filter (lambda (key) (string= name (string-remove-prefix ":" (symbol-name key)))) keys)))
+
+(defun global-interactive--find-value-in-vector-by-key (key the-vector)
+  (plist-get (car (seq-filter (lambda (item) (eq (car item) key)) the-vector)) key))
+
+(defun global-interactive-open-url (&optional url-expr)
+  "Interactively select url and open it"
+  (interactive)
+  (global-interactive-open-url-init)
+  (cond ((stringp url-expr) (shell-command (format "open \"%s\"" url-expr)))
+        ((vectorp url-expr) 
+         (lexical-let* ((keys (mapcar #'car url-expr))
+                        (selected-item (completing-read "Choose Url label: " (global-interactive--keys-to-names keys)))
+                        (key (global-interactive--find-key selected-item keys))
+                        (url-expr (global-interactive--find-value-in-vector-by-key key url-expr))
+                        )
+           (when url-expr
+             (message (format "fuckdsfsf:%s" url-expr))
+             (global-interactive-open-url url-expr))))
+        ((not url-expr) 
+         (lexical-let* 
+             ((url-plist global-interactive-url-plist)
+              (keys (seq-filter #'symbolp global-interactive-url-plist))
+              (selected-item (completing-read "Choose Url label: " (global-interactive--keys-to-names keys)))
+              (key (global-interactive--find-key selected-item keys))
+              (url-expr (plist-get url-plist key)))
+           (when url-expr
+             (global-interactive-open-url url-expr))))))
+
+(provide 'global-interactive-open-url)
+
+
+(plist-get global-interactive-url-plist :login)
+(seq-filter (lambda (value) (eq :login (car value))) (plist-get global-interactive-url-plist :login))
