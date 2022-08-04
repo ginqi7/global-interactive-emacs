2408cda2d17056ad5544392f0ddff3912ed3667e
jinqiqi <jinqiqi@ali-lurf.local>
2022-08-04 18:00:01 +0800

    feat: send_request.py use argparse parse args

--- global-interactive-send-request.el
+++ global-interactive-send-request.el
@@ -38,7 +38,8 @@
 
 
 (require 'global-interactive-emacs)
-(add-to-list 'global-interactive-default-command (list "Send Request" #'global-interactive-send-request))
+(add-to-list 'global-interactive-default-command
+             (list "Send Request" #'global-interactive-send-request))
 
 (defvar global-interactive-python-shell-path nil)
 
@@ -46,19 +47,35 @@
   "Simplify preview JSON if is too long."
   (let* ((json-str (json-serialize json)))
     (if (> (length json-str) 40)
-        (cond ((hash-table-p json) (format "%s...}" (substring json-str 0 40)))
-              ((vectorp json) (format "%s...]" (substring json-str 0 40)))
-              (t (format "%s..." (substring json-str 0 40))))
+        (cond
+         ((hash-table-p json)
+          (format "%s...}" (substring json-str 0 40)))
+         ((vectorp json)
+          (format "%s...]" (substring json-str 0 40)))
+         (t (format "%s..." (substring json-str 0 40))))
       json-str)))
 
 (defun global-interactive--json-to-candidates (json)
   "Convert JSON Item to interactive candidates."
   (let (candidates)
-    (cond ((hash-table-p json) (maphash (lambda (k v) (push (format "%s: %s" k (global-interactive--json-to-preview v)) candidates)) json))
-          ((vectorp json)
-           (let ((index -1))
-             (setq candidates (mapcar (lambda (item) (setq index (+ index 1)) (format "%s:%s" index (global-interactive--json-to-preview item))) json))))
-          (t (setq candidates (list json))))
+    (cond
+     ((hash-table-p json)
+      (maphash
+       (lambda (k v)
+         (push
+          (format "%s: %s" k (global-interactive--json-to-preview v))
+          candidates))
+       json))
+     ((vectorp json)
+      (let ((index -1))
+        (setq candidates
+              (mapcar
+               (lambda (item)
+                 (setq index (+ index 1))
+                 (format "%s:%s" index
+                         (global-interactive--json-to-preview item)))
+               json))))
+     (t (setq candidates (list json))))
     (append candidates (list global-interactive-back-to-upper-level))))
 
 (defun global-interactive--parse-json (last-json json)
@@ -67,25 +84,44 @@ LAST-JSON used to go back."
   (let ((json-str (json-serialize json)))
     (when (not (string= "{}" json-str))
       (kill-new (json-serialize json))))
-  
+
   (let* ((candidates (global-interactive--json-to-candidates json))
          (selected-item (completing-read "Response: " candidates))
          (key (car (split-string selected-item ":" t " +"))))
-    (cond ((string= key global-interactive-back-to-upper-level) (global-interactive--parse-json nil last-json))
-          ((hash-table-p json) (global-interactive--parse-json json (gethash key json)))
-          ((vectorp json) (global-interactive--parse-json json (elt json (string-to-number key)))))))
+    (cond
+     ((string= key global-interactive-back-to-upper-level)
+      (global-interactive--parse-json nil last-json))
+     ((hash-table-p json)
+      (global-interactive--parse-json json (gethash key json)))
+     ((vectorp json)
+      (global-interactive--parse-json json
+                                      (elt json
+                                           (string-to-number key)))))))
 
 (defun global-interactive-send-request ()
   "Send request with chrome cookie."
   (interactive)
   (if global-interactive-python-shell-path
-      (let* ((method (completing-read "Selected Request Method: " '("GET" "POST")))
-             (url (global-interactive-read "Please Input URL: " '("Please Input URL:")))
-             (body (when (string= method "POST") (global-interactive-read "Please Input Body: " '("Please Input Body: "))))
-             (command (format "python3 %s %s %s '%s'" global-interactive-python-shell-path method url body))
+      (let* ((method
+              (completing-read "Selected Request Method: "
+                               '("GET" "POST")))
+             (url
+              (global-interactive-read "Please Input URL: "
+                                       '("Please Input URL:")))
+             (body
+              (when (string= method "POST")
+                (global-interactive-read "Please Input Body: "
+                                         '("Please Input Body: "))))
+             (command
+              (if body
+                  (format "python3 %s '%s' '%s' --body '%s'" global-interactive-python-shell-path method url body)
+                (format "python3 %s '%s' '%s'" global-interactive-python-shell-path method url)))
              (response (shell-command-to-string command)))
-        (global-interactive--parse-json nil (json-parse-string response)))
-    (completing-read "Warnning" '("Please config 'global-interactive-python-shell-path'"))))
+        (print response)
+        (global-interactive--parse-json nil
+                                        (json-parse-string response)))
+    (completing-read "Warnning"
+                     '("Please config 'global-interactive-python-shell-path'"))))
 
 (provide 'global-interactive-send-request)
 ;;; global-interactive-send-request.el ends here
--- send_request.py
+++ send_request.py
@@ -5,23 +5,40 @@ import sys
 from pycookiecheat import chrome_cookies
 import json
 import requests
+from requests.auth import HTTPBasicAuth
+
+import argparse
+ 
+parser = argparse.ArgumentParser(description="Send a request",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("method", help="Request method")
+parser.add_argument("url", help="Request URL")
+parser.add_argument("-b", "--body", help="Request Body", required=False)
+parser.add_argument("-u", "--user", help="Request User", required=False)
+parser.add_argument("-p", "--password", help="Request Password", required=False)
+args = parser.parse_args()
+config = vars(args)
 
 
-if (len(sys.argv) < 3):
-    raise Exception("There need least 3 arguments") 
-
-method = sys.argv[1]
-url = sys.argv[2]
-
+method = config.get("method")
+url = config.get("url")
+user = config.get("user")
+passwod = config.get("password")
+body = config.get("body")
 cookies = chrome_cookies(url)
 
+
+
 if method.lower() == 'get':
-    response = requests.get(url=url, cookies=cookies)
+    response = requests.get(url=url, cookies=cookies, auth=HTTPBasicAuth(user, passwod))
 elif method.lower() == 'post':
-    body = NONE
-    if (len(sys.argv) >= 4):
-        body = sys.argv[3]
-    response = requests.post(url=url, cookies=cookies, json=json.loads(body))
+    response = requests.post(url=url, cookies=cookies, json=json.loads(body), auth=HTTPBasicAuth(user, passwod))
 else:
     raise Exception("Sorry, not support the method") 
-print(json.dumps(response.json()))
+contentType = response.headers['content-type']
+if  "text/html" in contentType :
+    print(response.text)
+elif  "application/json" in contentType :
+    print(json.dumps(response.json()))
+else :
+    raise Exception("Sorry, not support the response contentType: " + contentType) 
